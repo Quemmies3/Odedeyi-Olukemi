ffccafc8be40f49f772f09eacc9182bb04ccfe73
Quemmies3 <quemestree@gmail.com>
2022-05-14 14:21:32 +0100

    modified component file structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import About from './components/about';
+import Experience from './components/experience';
+import Education from './components/education';
+import Skills from './components/skills';
+import Interests from './components/interests';
+import Contact from './components/contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <About />
+      <Experience />
+      <Education />
+      <Skills />
+      <Interests />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/about.js
@@ -0,0 +1,10 @@
+
+
+
+export default function About () {
+    return (
+        <div >
+            <h2 className="text-blue">Hello World!</h2>
+        </div>
+    )
+}
