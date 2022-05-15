633fc1b3abe12ffc93a3127a5be6c76b4f74e618
Quemmies3 <quemestree@gmail.com>
2022-05-15 23:30:26 +0100

    worked on the about and contact page

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
+// import Profile from './components/images/profile.png';
 
 
 export default function About () {
     return (
-        <div >
-            <h2 className="text-blue">Hello World!</h2>
-        </div>
+        <section id="about">
+            <div>
+                {/* <img src="" alt="profile"/> */}
+                <div>
+                    <p>Reliable, dedicated and user-focused Frontend Developer <br></br>
+                    with eye for design and deep understanding of usability goals with experience<br></br>
+                    and strong professional background in import logistics, customer service/client relationship and satisfaction, product quality assurance.<br></br> 
+                    Skilled at managing time, projects, and resources to accomplish development goals, and also marries functional requirements and creative elements to produce optimal interfaces.</p>
+                </div>
+            </div>
+        </section>
     )
 }
